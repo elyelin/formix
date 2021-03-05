79d4f07c5cd4ed7109abde2c200eb78472be2982
Elyelin Carrasquero <elyelin15@gmail.com>
2021-03-05 18:04:25 -0300

    cambios

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Formik, useFormikContext, useField } from "formik";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import * as Yup from "yup";
+
+const MyInput = ({ fieldName, ...props }) => {
+  const [field, meta] = useField(fieldName);
+  return (
+    <>
+      <TextInput
+        style={styles.input}
+        onChangeText={field.onChange("fieldName")}
+        onBlur={field.onBlur(fieldName)}
+        value={field.value}
+        {...props}
+      />
+      {meta.error && meta.touched && (
+        <Text style={{ color: "red" }}>{meta.error}</Text>
+      )}
+    </>
+  );
+};
+
+const EmailForm = () => {
+  const { submitForm } = useFormikContext();
+  return (
+    <>
+      <Text>Correo electrónico</Text>
+      <MyInput fieldName="email" />
+      <MyInput fieldName="name" />
+      <Button onPress={submitForm} title="Enviar" />
+    </>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Formik
+        onSubmit={(x) => console.log(x)}
+        validationSchema={Yup.object({
+          email: Yup.string().email("Correo inválido").required("Requerido"),
+          name: Yup.string().min(10).required("Requerido"),
+        })}
+        initialValues={{ email: "", name: "" }}
+      >
+        <EmailForm />
+      </Formik>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 30,
+    width: 140,
+    backgroundColor: "#eee",
   },
 });
